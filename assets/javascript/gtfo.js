5839f5e8841c7a05b536852eb73c6ae5dbf7e690
Adam Griepentrog <adammiltron@gmail.com>
2018-08-26 18:41:16 -0500

    pushed Saturday's code with working polyline

--- assets/javascript/gtfo.js
+++ assets/javascript/gtfo.js
@@ -1,64 +1,81 @@
     
-    // var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.983758,-93.180473&radius=1500&type=restaurant&key=AIzaSyAUCRBjRZyAYWMlrjubm_7G1B5m6468KTc";    
-      var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 93.265, lng: 44.977},
-          zoom: 14
+    // var queryUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.983758,-93.180473&radius=1500&type=restaurant&key=AIzaSyAUCRBjRZyAYWMlrjubm_7G1B5m6468KTc&libraries=places&callback=map";
+    var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAUCRBjRZyAYWMlrjubm_7G1B5m6468KTc&libraries=places&callback=map";
+    var pos
+    var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 93.265, lng: 44.977},
+        zoom: 14
+      });
+      infoWindow = new google.maps.InfoWindow;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          var marker = new google.maps.Marker({
+      position: pos,
+      title: "You are here"
+    
+    });
+
+    poly = new google.maps.Polyline({
+        strokeColor: '#000000',
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+      });
+      poly.setMap(map);
+
+      map.addListener('click', addLatLng);
+    
+
+    // Handles click events on a map, and adds a new point to the Polyline.
+    function addLatLng(event) {
+      var path = poly.getPath();
+
+      // Because path is an MVCArray, we can simply append a new coordinate
+      // and it will automatically appear.
+      path.push(event.latLng);
+
+      // Add a new marker at the new plotted point on the polyline.
+      var marker = new google.maps.Marker({
+        position: event.latLng,
+        title: '#' + path.getLength(),
+        map: map
+      });
+    }
+    
+    marker.setMap(map);
+    map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
         });
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            var marker = new google.maps.Marker({
-                position: pos,
-                title: "You are here"
-        
-              });
-              marker.setMap(map);
-            //create radius from a location bias from api
-            //pull restaurant from Query URL 
-            //create another geolocation for restaurant marker - drop marker
-            //confirm or deny restaurant location via button
-            //pull random restaurant from array Query URL if denied 
-            //poly line creates line between location and restaurant 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent();
-            // infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
       }
+    
+      
+    }
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
 
-     
+   
 
-    //   $.ajax({
-    //     url: queryURL,
-    //     method: 'GET'
-    // })
-    // .done(function(response) {
-        
+    }
 
-    //     console.log(queryURL);
 
-    //     console.log(response);
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUCRBjRZyAYWMlrjubm_7G1B5m6468KTc&callback=initMap">
+  </script>
     
     
